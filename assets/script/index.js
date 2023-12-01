5da38aba69d67e56897bdb711fbcd4fb4bb461ee
MC Guenette <mc.c.guenette@gmail.com>
2023-11-30 22:53:22 -0600

    added Score class and functionality

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-import { onEvent, select, selectAll } from './utils.js';
+import { onEvent, select } from './utils.js';
+import { Score } from './Score.js';
 
 const modal = select('.modal');
 const frontModal = select('.modal-front');
@@ -9,39 +10,43 @@ const overlay = select('.overlay');
 const closeModalBtn = select('.modal-close');
 const startBtn = select('#start-btn');
 const countdownStart = select('#countdown-start');
-const countdownMessage = select('#countdown-message');
 
-const openFrontModal = function () {
+let words;
+let currentWordIndex;
+let correctWordCount;
+let countdownInterval;
+
+const openStartModal = function () {
     frontModal.classList.remove('hidden');
     backModal.classList.add('hidden');
     overlay.classList.remove('hidden');
     modal.style.display = 'flex';
 };
 
-const switchToBackModal = function () {
+const switchToGameModal = function () {
     frontModal.classList.add('hidden');
     backModal.classList.remove('hidden');
     modal.style.display = 'flex';
-    startCountdown();
+    startModalCountdown();
 };
 
-const closeFrontModal = function () {
+const closeStartModal = function () {
     frontModal.classList.add('hidden');
     overlay.classList.add('hidden');
     modal.style.display = 'none';
 };
 
-const closeBackModal = function () {
+const closeGameModal = function () {
     backModal.classList.add('hidden');
     overlay.classList.add('hidden');
     modal.style.display = 'none';
 };
 
-const startCountdown = function () {
+const startModalCountdown = function () {
     let count = 3;
     countdownStart.textContent = count;
 
-    const countdownInterval = setInterval(() => {
+    countdownInterval = setInterval(() => {
         count--;
 
         if (count > 0) {
@@ -50,22 +55,104 @@ const startCountdown = function () {
             countdownStart.textContent = 'GO!';
         } else {
             clearInterval(countdownInterval);
-            closeBackModal();
+            closeGameModal();
+            startGame();
         }
     }, 1000);
 };
 
+let timerInterval;
+let remainingSeconds;
+
+const updateTimer = function () {
+    remainingSeconds--;
+
+    if (remainingSeconds >= 0) {
+        select('#timer').textContent = remainingSeconds;
+    } else {
+        clearInterval(timerInterval);
+        endGame();
+    }
+};
+
+const startTimer = function () {
+    remainingSeconds = 99;
+    select('#timer').textContent = remainingSeconds;
+
+    timerInterval = setInterval(updateTimer, 1000);
+};
+
+const startGame = function () {
+    words = [
+        'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
+        'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
+        'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
+        'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
+        'philosophy', 'database', 'periodic', 'capitalism', 'abominable',
+        'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada',
+        'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'promise',
+        'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake',
+        'baseball', 'beyond', 'evolution', 'banana', 'perfume', 'computer',
+        'management', 'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess',
+        'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library',
+        'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy',
+        'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous',
+        'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+        'keyboard', 'window', 'beans', 'truck', 'sheep', 'band', 'level', 'hope',
+        'download', 'blue', 'actor', 'desk', 'watch', 'giraffe', 'brazil', 'mask',
+        'audio', 'school', 'detective', 'hero', 'progress', 'winter', 'passion',
+        'rebel', 'amber', 'jacket', 'article', 'paradox', 'social', 'resort', 'escape'
+    ];
+
+    currentWordIndex = 0;
+    correctWordCount = 0;
+
+    displayCurrentWord();
+    startTimer();
+};
+
+const displayCurrentWord = function () {
+    const wordOutput = select('#word-output');
+    wordOutput.textContent = words[currentWordIndex];
+};
+
+const checkUserInput = function () {
+    const userInput = select('#tap-touche').value.toLowerCase().trim();
+    const currentWord = words[currentWordIndex].toLowerCase();
+
+    if (userInput === currentWord) {
+        correctWordCount++;
+
+        if (currentWordIndex < words.length - 1) {
+            currentWordIndex++;
+            displayCurrentWord();
+        } else {
+            endGame();
+        }
+
+        // Clear the input field
+        select('#tap-touche').value = '';
+    }
+};
+
+const endGame = function () {
+    clearInterval(countdownInterval);
+    const remainingSeconds = select('#timer').textContent;
+    const score = new Score(new Date(), correctWordCount, (correctWordCount / words.length) * 100);
+};
+
 onEvent('keydown', document, function (e) {
     if (e.key === 'Escape' && !frontModal.classList.contains('hidden')) {
-        closeFrontModal();
+        closeStartModal();
     }
 });
 
-onEvent('click', overlay, closeFrontModal);
-onEvent('click', closeModalBtn, closeFrontModal);
-onEvent('click', startBtn, switchToBackModal);
+onEvent('click', overlay, closeStartModal);
+onEvent('click', closeModalBtn, closeStartModal);
+onEvent('click', startBtn, switchToGameModal);
+onEvent('input', select('#tap-touche'), checkUserInput);
 
 // Initial modal display
 setTimeout(() => {
-    openFrontModal();
+    openStartModal();
 }, 100);
